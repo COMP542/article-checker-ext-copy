a31739ece6324c684f1a82e83b6f14e92c39be9a
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2026-04-21 19:27:44 -0700

    Changed Python Call
    python3 -> python
    for consistency and compatibility (I don't use python3 on my machine)
    Should still work, let me know if this breaks on your machine

--- backend/run.sh
+++ backend/run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if [ ! -d ".venv" ]; then
-  python3 -m venv .venv
+  python -m venv .venv
   . .venv/bin/activate
   python -m pip install --upgrade pip
   if [ -f requirements.txt ]; then
