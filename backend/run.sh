af7ddec551e2c72b8344645810f9cb0ff6d60b36
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2026-04-21 19:37:17 -0700

    Changed Virtual Environment Activation Call
    The directory of the 'activate' file changes depending on the OS, the activate_venv() accounts for that discrepency

--- backend/run.sh
+++ backend/run.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Activate virtual environment based on OS type
+activate_venv() {
+  if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    . .venv/Scripts/activate
+  else
+    # Assumes a Unix-like OS (Linux, macOS) if not Windows
+    . .venv/bin/activate
+  fi
+}
+
+
 if [ ! -d ".venv" ]; then
   python -m venv .venv
-  . .venv/bin/activate
+  activate_venv
   python -m pip install --upgrade pip
   if [ -f requirements.txt ]; then
     pip install -r requirements.txt
   fi
 else
-  . .venv/bin/activate
+  activate_venv
 fi
 
 python app.py
