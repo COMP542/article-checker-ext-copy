6b108df6046ca56ab4c6dc7b60283b71b537bbb5
GevChalikyan <72428068+GevChalikyan@users.noreply.github.com>
2026-04-28 13:49:04 -0700

    Rerouted Request to Remote Server for Analysis

--- extension/popup.js
+++ extension/popup.js
@@ -49,7 +49,7 @@ async function extractFromPage(tabId) {
 }
 
 async function postToBackend(payload) {
-  const res = await fetch("http://127.0.0.1:5000/analyze", {
+  const res = await fetch("http://104.248.67.141:5000/analyze", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload)
